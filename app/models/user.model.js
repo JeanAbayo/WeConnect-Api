b14f99ba0088399daca6ced5c545fcf3f6b0e7a6
Jean Abayo <abagammy@gmail.com>
2018-02-28 21:01:25 +0300

    Validate email and username

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -3,7 +3,20 @@ const mongoose = require("mongoose");
 const UserSchema = mongoose.Schema(
 	{
 		username: String,
-		email: String,
+		username: {
+			type: String,
+			lowercase: true,
+			required: [true, "can't be blank"],
+			match: [/^[a-zA-Z0-9]+$/, "is invalid"],
+			index: true
+		},
+		email: {
+			type: String,
+			lowercase: true,
+			required: [true, "can't be blank"],
+			match: [/\S+@\S+\.\S+/, "is invalid"],
+			index: true
+		},
 		password: String
 	},
 	{
