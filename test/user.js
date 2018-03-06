784e255b3dc14569aa02f329db88f76a611eb64c
Jean Abayo <abagammy@gmail.com>
2018-03-06 16:43:21 +0300

    [Chore #155546062] Add api versioning
    Add also a failing auth test

--- test/user.js
+++ test/user.js
@@ -1,4 +1,3 @@
-let user = require('./utils/data.json').user;
 //During the test the env variable is set to test
 process.env.NODE_ENV = 'test';
 
@@ -11,29 +10,47 @@ let chaiHttp = require('chai-http');
 let api = require('../server');
 let should = chai.should();
 
+let user = require('./utils/data.json').user;
+
 chai.use(chaiHttp);
 //Our parent block
 describe('Users', () => {
-    beforeEach((done) => { //Before each test we empty the database
-        User.remove({}, (err) => { 
-           done();         
-        });     
+  beforeEach((done) => { //Before each test we empty the database
+    User.remove({}, (err) => {
+      done();
     });
-/*
-* Test user registration
-*/
+  });
+  /*
+  * Test user registration
+  */
   describe('User registration', () => {
       it('it should register a user', (done) => {
         chai.request(api)
-            .post('/api/auth/register')
+            .post('/auth/register')
             .send(user)
             .end((err, res) => {
                 res.should.have.status(201);
                 res.body.should.be.a('object');
-                res.body.should.have.property('message').eql('User account created successfuly');
+                res.body.should.have.property('message')
+                .eql('User account created successfuly');
               done();
             });
       });
   });
-
+  /*
+  * Test user can authenticate
+  */
+  describe('User can authenticate', () => {
+    it('it should authenticate a user successfuly', (done) => {
+      chai.request(api)
+        .post('/auth/login')
+        .send({email: "John Doe", password: "secret"})
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('message')
+          .eql('User logged in successfuly');
+        });
+    });
+  });
 });
