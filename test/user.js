3b1e18fd47ffa01a0245fe2b95227315b2b31c64
Jean Abayo <abagammy@gmail.com>
2018-03-08 15:48:34 +0300

    [Chore 155546062] Test invalid auth
    Need to figure out how to test Success Auth though

--- test/user.js
+++ test/user.js
@@ -1,56 +1,61 @@
 //During the test the env variable is set to test
-process.env.NODE_ENV = 'test';
+process.env.NODE_ENV = "test";
 
 let mongoose = require("mongoose");
-let User = require('../api/models/User');
+let User = require("../api/models/User");
 
 //Require the dev-dependencies
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-let api = require('../server');
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let api = require("../server");
 let should = chai.should();
 
-let user = require('./utils/data.json').user;
+let user = require("./utils/data.json").user;
 
 chai.use(chaiHttp);
 //Our parent block
-describe('Users', () => {
-  beforeEach((done) => { //Before each test we empty the database
-    User.remove({}, (err) => {
+describe("Users", () => {
+  beforeEach(done => {
+    //Before each test we empty the database
+    User.remove({}, err => {
       done();
     });
   });
   /*
   * Test user registration
   */
-  describe('User registration', () => {
-      it('it should register a user', (done) => {
-        chai.request(api)
-            .post('/auth/register')
-            .send(user)
-            .end((err, res) => {
-                res.should.have.status(201);
-                res.body.should.be.a('object');
-                res.body.should.have.property('message')
-                .eql('Your account was created successfuly');
-              done();
-            });
-      });
+  describe("User registration", () => {
+    it("it should register a user", done => {
+      chai
+        .request(api)
+        .post("/auth/register")
+        .send(user)
+        .end((err, res) => {
+          res.should.have.status(201);
+          res.body.should.be.a("object");
+          res.body.should.have
+            .property("message")
+            .eql("Your account was created successfuly");
+          done();
+        });
+    });
   });
   /*
   * Test user can authenticate
   */
-  describe('User can authenticate', () => {
-    it('it should authenticate a user successfuly', (done) => {
-      chai.request(api)
-        .post('/auth/login')
-        .send({email: "johndoe@test.com", password: "secret"})
+  describe("User can authenticate", () => {
+    it("it should not authenticate invalid data", done => {
+      chai
+        .request(api)
+        .post("/auth/login")
+        .send({ email: "fakeuser@test.com", password: "fake" })
         .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a('object');
-          res.body.should.have.property('token');
-          res.body.should.have.property('message')
-          .eql('User logged in successfuly');
+          res.should.have.status(422);
+          res.body.should.be.a("object");
+          res.body.should.have
+            .property("message")
+            .eql("User does not exist, check your email input");
+          done();
         });
     });
   });
